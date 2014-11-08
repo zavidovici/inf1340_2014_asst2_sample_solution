ed5774d18a633d7a1957d2c635260a05d100f2b9
Sasa Milic <milic@cs.toronto.edu>
2014-11-07 19:25:38 -0500

    Added additional tests.

--- test_papers.py
+++ test_papers.py
@@ -14,16 +14,109 @@ __status__ = "Prototype"
 import pytest
 from papers import decide
 
-
-def test_basic():
-    assert decide("test_returning_citizen.json", "watchlist.json", "countries.json") == ["Accept", "Accept"]
-    assert decide("test_watchlist.json", "watchlist.json", "countries.json") == ["Secondary"]
-    assert decide("test_quarantine.json", "watchlist.json", "countries.json") == ["Quarantine"]
+DIR = "test_jsons/"
 
 
-def test_files():
+def test_returning():
+    """
+    Travellers are returning to KAN.
+    """
+    assert decide(DIR + "test_returning_citizen.json", DIR + "watchlist.json", DIR + "countries.json") ==\
+        ["Accept", "Accept", "Quarantine"]
+
+
+def test_watchlist1():
+    """
+    Travellers should have a secondary screening.
+    """
+    # If the traveller has a name or passport on the watch list,
+    # she or he must be sent to secondary processing.
+    assert decide(DIR + "test_watchlist1.json", DIR + "watchlist.json", DIR + "countries.json") ==\
+        ["Secondary", "Secondary"]
+
+
+def test_watchlist2():
+    """
+    Only one of first or last name is on watchlist.
+    """
+    assert decide(DIR + "test_watchlist2.json", DIR + "watchlist.json", DIR + "countries.json") ==\
+        ["Accept", "Accept"]
+
+
+def test_quarantine():
+    assert decide(DIR + "test_quarantine.json", DIR + "watchlist.json", DIR + "countries.json") ==\
+        ["Quarantine", "Accept"]
+
+
+def test_quarantine_via():
+    """
+    Test cases where traveller is coming via country with a medical advisory.
+    """
+    assert decide(DIR + "test_quarantine_via.json", DIR + "watchlist.json", DIR + "countries.json") ==\
+        ["Quarantine"]
+
+
+def test_incomplete():
+    """
+    If the required information for an entry record is incomplete, the traveler must be rejected.
+    """
+    assert decide(DIR + "test_incomplete.json", DIR + "watchlist.json", DIR + "countries.json") == \
+        ["Reject", "Reject", "Reject"]
+
+
+def test_invalid_visa():
+    """
+    Travellers with invalid visas.
+    """
+    assert decide(DIR + "test_invalid_visa.json", DIR + "watchlist.json", DIR + "countries.json") == \
+        ["Reject", "Reject", "Reject", "Reject", "Reject"]
+
+
+def test_valid_visa():
+    """
+    Travellers with invalid visas.
+    """
+    assert decide(DIR + "test_valid_visa.json", DIR + "watchlist.json", DIR + "countries.json") == \
+        ["Accept"]
+
+
+def test_visa_not_needed():
+    """
+    Non-returning travellers not needing visas.
+    """
+    assert decide(DIR + "test_visa_not_needed.json", DIR + "watchlist.json", DIR + "countries.json") == \
+        ["Accept", "Accept"]
+
+
+def test_case_insensitivity():
+    """
+    Country codes and passports with mixed cases.
+    """
+    assert decide(DIR + "test_case_insensitivity.json", DIR + "watchlist.json", DIR + "countries.json") == \
+        ["Accept", "Secondary"]
+
+
+def test_conflicts1():
+    """
+    """
+    assert decide(DIR + "test_conflicts1.json", DIR + "watchlist.json", DIR + "countries.json") == \
+        ["Quarantine", "Reject", "Secondary"]
+
+
+def test_conflicts2():
+    """
+    """
+
+    # Conflicts should be resolved according the order of priority:
+    # quarantine, reject, secondary, and accept.
+
+    assert decide(DIR + "test_conflicts2.json", DIR + "watchlist.json", DIR + "countries.json") == \
+        ["Reject"]
+
+
+def test_error_file_not_found():
     with pytest.raises(FileNotFoundError):
-        decide("test_returning_citizen.json", "", "countries.json")
+        decide(DIR + "test_returning_citizen.json", "", "countries.json")
 
 # add functions for other tests
-
+# TEST ORIGINAL FILES NOT CHANGED
